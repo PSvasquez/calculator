6d2850b9c0b5e6f5a7a5dc561ba1cd7c51e46ff9
PSVasquez <patriciasantana199922@gmail.com>
2016-08-05 14:42:34 -0400

    Adding to Portfolio

---
+++ functions.js
@@ -0,0 +1,41 @@
+function divide(x,y)
+{
+  return x/y;
+}
+
+// TODO: You must implement the rest of the functions.
+
+function multiply(x,y)
+{
+return x*y;
+
+}
+
+function add(x,y)
+{
+return x+y;
+
+}
+
+//x is the base number, y is the exponent
+//hint: check out Math.pow
+function power(x,y)
+{
+return Math.pow(x,y);
+
+}
+
+function square_root(x)
+{
+return Math.sqrt(x);
+}
+
+function logBase10(x)
+{
+return Math.log(x);
+}
+
+function generateRandomNumber()
+{
+return Math.random();
+}
